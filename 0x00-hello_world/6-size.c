3f48b69abf4f56290da67549e3c7cd47084af49d
Tinashe_nash <tinashemudede05@gmail.com>
2022-09-10 14:16:22 -0700

    program that prints exactly "Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long li;
