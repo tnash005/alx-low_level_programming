d7396f3f811f3473eddb9b5e96b0aa625d806260
Tinashe_nash <tinashemudede05@gmail.com>
2022-09-15 04:49:43 -0700

    this main file is a test for a postitive_or_negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - tests function if it prints positive or negative interger
+ *  Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Tasks 0x03.c debugging
