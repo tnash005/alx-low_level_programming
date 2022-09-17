f9700be4a17363ba62b1579af256cfbffe2a2db3
Tinashe_nash <tinashemudede05@gmail.com>
2022-09-16 22:18:11 -0700

    main file to test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * positive_or_negative - testes if an interger is positive or negative
- * @i: parameter to be tested
+ * main  - testes if an interger is positive or negative
  *  Return: 0
  *
  */
-void positive_or_negative(int i)
+int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
