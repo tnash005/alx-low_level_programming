ea2790356af4f7b2302659e393320c0e34c6f862
Tinashe_nash <tinashemudede05@gmail.com>
2022-09-19 04:40:07 -0700

    hot fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,16 +8,16 @@
 
 void puts2(char *str)
 {
-	int i;
+int i;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		 if (i % 2 == 0)
-		 {
-			 putchar(str[i]);
-		 }
-		i++;
-	}
-	putchar('\n');
+i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+putchar(str[i]);
+}
+i++;
+}
+putchar('\n');
 }
