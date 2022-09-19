994e3401ad73cc93db623604501168775240efce
Tinashe_nash <tinashemudede05@gmail.com>
2022-09-19 12:23:10 -0700

    Write a function that convert a string to an intege

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _atoi - convert string to an interger
+ * @s: pointer to a string
+ *
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int sign;
+	unsigned int num;
+	char *temp;
+
+	temp = s;
+	num = 0;
+	sign = i;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '_')
+			sign *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
+}
