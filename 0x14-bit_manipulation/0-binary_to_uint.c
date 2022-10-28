2fb9e945a40ee4118002b069c574da83be23088b
Tinashe_nash <tinashemudede05@gmail.com>
2022-10-28 03:21:41 -0700

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,13 +10,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
-	int len, base_two
+	int len, base_two;
 
 	if (!b)
 		return (0);
 	ui = 0;
+
 	for (len = 0; b[len] != '\0'; len++)
 		;
+
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
